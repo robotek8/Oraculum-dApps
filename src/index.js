8b5da720808d3d65a9287dabed1c7e13a3e0a424
unknown <roboteck8@gmail.com>
2022-04-15 19:05:27 +0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import { Signer } from '@waves/signer';
+import { ProviderWeb } from '@waves.exchange/provider-web';
+import { nodeInteraction } from "@waves/waves-transactions";
+
+
+const nodeUrl = 'https://nodes-testnet.wavesnodes.com';
+const OraculumAddress = '3N5XNq7FMSGU5NmZbLFu8HkDY8EDYubTJwy';
+
+const signer = new Signer({NODE_URL: nodeUrl});
+const provider = new ProviderWeb('https://testnet.waves.exchange/signer/')
+
+signer.setProvider(provider);
+
+document.querySelector(".js-invoke").addEventListener("click", async function () {
+    try {
+        let question = document.getElementById('questionInput').value + Date.now();
+        console.log('The question is '+ question);
+
+        const user = await signer.login();
+        document.querySelector(".address").innerHTML = `Your address is: ${user.address}`;
+
+        // Call tellme function of wavesexplorer.com/tesnet/address/3N5XNq7FMSGU5NmZbLFu8HkDY8EDYubTJwy/script dApp
+        // Generate an answer and write it to the dApp data storage
+
+        try {
+            await signer.invoke({
+                dApp: OraculumAddress,
+                call: {
+                    function: "tellme",
+                    args:[{"type": "string", "value": question}]
+                }
+            }).broadcast({confirmations: 1}).then(resp => console.log(resp));
+
+        // Read an answer from dApp data storage
+
+            let answer = await nodeInteraction.accountDataByKey(user.address+'_a',OraculumAddress,nodeUrl);
+            document.querySelector(".answer").innerHTML = `Your answer is: ${answer.value}`;
+        } catch (e) {
+            console.error('Question denied')
+        }; 
+
+    } catch (e) {
+        console.error('Login rejected')
+    };
+
+});
